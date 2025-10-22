9aae6659a1839a35e02bd90ff734d154e253706f
Balaji7904096 <720823106018@hit.edu.in>
2025-10-22 10:52:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function showWelcomeMessage() {
+    alert("Welcome to Goa Tourism!");
+}
+
+function learnMore() {
+    alert("Goa is known for its beautiful beaches, churches, forts, and vibrant nightlife!");
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    alert("Thank you for contacting us! We’ll get back to you soon.");
+}
